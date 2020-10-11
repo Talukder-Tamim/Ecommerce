670b41ac0e96c907422b66a82d4b82385877559b
Talukder-Tamim <talukder.tamim@gmail.com>
2020-10-11 10:27:34 +0600

    Eshop

---
+++ store/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . models import Product, Category
+
+
+class AdminProduct(admin.ModelAdmin):
+	list_display = ['name', 'price', 'category']
+
+
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category)
---
+++ store/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.IntegerField(default=0)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	description = models.CharField(max_length=200, default='', blank=True, null=False)
+	image = models.ImageField(upload_to='uploads/products/')
+
+	def __str__(self):
+		return self.name
+
+
