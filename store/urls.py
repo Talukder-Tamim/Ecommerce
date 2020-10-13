3dbd020058b01990532f23e29ad06f36a715661e
Talukder-Tamim <talukder.tamim@gmail.com>
2020-10-13 11:02:01 +0600

    updated

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('/category/<ctg_name>', views.category_post, name="category-post")
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from . models import Product
+from . models import Product, Category
 
 
 def index(request):
 	products = Product.objects.all()
-	context = {'products': products}
+	categories = Category.objects.all()
+	context = {'products': products, 'categories': categories}
 	return render(request, 'index.html', context)
+
+
+def category_post(request, ctg_name):
+    category_obj = Category.objects.get(name=ctg_name)
+    category_post = Product.objects.filter(category=category_obj)
+    context = {'categories': category_post}
+    return render(request, 'category_post.html', context)
+
